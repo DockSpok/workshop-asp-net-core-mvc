1d9bf46bc9c233bc6b8413ee730c93fbde5496a4
Mauro DockSpok <mauro.energiaboa@gmail.com>
2019-01-21 17:49:39 -0200

    First Model-Controller-View - Department

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+// Esta clase foi criada usando o Add New Controller
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department { Id = 1, Name = "Electronics" });
+            list.Add(new Department { Id = 2, Name = "Fashion" });
+            // Enviando a lista para o front end.
+            return View(list);
+        }
+    }
+}
---
+++ SalesWebMvc/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿
+namespace SalesWebMvc.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        /*
+         * ctor
+        public Department()
+        {
+
+        }
+        */
+        
+    }
+}
